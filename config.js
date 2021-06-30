06253249015606f9e1fc6b13bb84207e652be5da
Namdzdethun <86723806+Namdzdethun@users.noreply.github.com>
2021-06-30 19:52:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,13 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'bạn à!',
+    introDesc: `Đây là câu hỏi dành cho bạn`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'tớ hỏi thật',
+    desc: 'tớ đẹp zai nhỉ ',
+    btnYes: 'đương nhiên rồi',
+    btnNo: 'Không bạn',
+    question:'Nam đẹp trai nhất thế giới nhỉ',
+    btnReply: 'Gửi cho Nam',
+    reply: 'đương nhiên rồi hihi',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
